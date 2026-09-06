8f7ce930ce056817eb106ebcc38eaf5e593b170e
Marcuth <thiago.playgames15@gmail.com>
2026-09-06 17:44:57 -0300

    feat: implement daily synchronization script to populate and update dragon data from local JSON files to Prisma database

--- src/dbgames/sync.ts
+++ src/dbgames/sync.ts
@@ -88,7 +88,7 @@ async function main() {
                 isFullData: false,
                 ...(blacklistedIds.length > 0 ? { id: { notIn: blacklistedIds } } : {})
             },
-            take: 50,
+            take: 10,
             orderBy: { id: "asc" }
         })
 
