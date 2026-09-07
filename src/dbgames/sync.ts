6f650ebba480445b7f4a6d64aaa5a10908269fa2
Marcuth <thiago.playgames15@gmail.com>
2026-09-07 13:37:26 -0300

    Update sync.ts

--- src/dbgames/sync.ts
+++ src/dbgames/sync.ts
@@ -88,7 +88,7 @@ async function main() {
                 isFullData: false,
                 ...(blacklistedIds.length > 0 ? { id: { notIn: blacklistedIds } } : {})
             },
-            take: 10,
+            take: 100,
             orderBy: { id: "asc" }
         })
 
